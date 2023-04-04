12af9345b8815ca9ec8f9cae4d4cf3482435238e
William Clifford <w.big.red.dog@gmail.com>
2023-04-04 03:34:01 -0500

    Completed javadoc comments for MainActivity and StarshipActivity classes

--- app/src/main/java/edu/utsa/cs3443/rye747_lab4/MainActivity.java
+++ app/src/main/java/edu/utsa/cs3443/rye747_lab4/MainActivity.java
@@ -8,8 +8,23 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import edu.utsa.cs3443.rye747_lab4.controller.MainController;
 
+/**
+ * This class represents MainActivity objects.
+ * Each MainActivity object is instantiated with a Bundle object
+ * MainActivity objects are responsible for initializing the activity_main layout
+ *
+ * @author William Clifford (rye747)
+ * UTSA CS 3443 - Lab 4
+ * Spring 2023
+ */
 public class MainActivity extends AppCompatActivity {
     private MainController controller;
+
+    /**
+     * This method is responsible for initializing the application view, and instantiating the button views for the activity_main layout
+     *
+     * @param savedInstanceState a Bundle class object that is passed into the inherited class onCreate method
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,6 +40,11 @@ public class MainActivity extends AppCompatActivity {
         setupButton(enterprise_2);
     }
 
+    /**
+     * This method initializes the view matching the specified id as a Button class object with an onClickListener
+     *
+     * @param view the view that is assigned an OnClickListener
+     */
     public void setupButton(View view){
         view.setOnClickListener(controller);
     }
--- app/src/main/java/edu/utsa/cs3443/rye747_lab4/StarshipActivity.java
+++ app/src/main/java/edu/utsa/cs3443/rye747_lab4/StarshipActivity.java
@@ -6,10 +6,25 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import edu.utsa.cs3443.rye747_lab4.controller.StarshipController;
 
+/**
+ * This class represents StarshipActivity objects.
+ * Each StarshipActivity object is instantiated with a Bundle object
+ * StarshipActivity objects are responsible for initializing the activity_starship layout
+ *
+ * @author William Clifford (rye747)
+ * UTSA CS 3443 - Lab 4
+ * Spring 2023
+ */
 public class StarshipActivity extends AppCompatActivity{
-        private StarshipController controller;
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
+    private StarshipController controller;
+
+    /**
+     * This method is responsible for initializing the application view for the activity_starship layout
+     *
+     * @param savedInstanceState a Bundle class object that is passed into the inherited class onCreate method
+     */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             setContentView(R.layout.activity_starship);
 
